0dadbb7d9fa7240b88ab113ba6393537705bc456
Lukas Gräf <lukas.graef@vector.com>
2024-12-11 12:55:52 +0100

    Reproduce JSpecify Problem in tests

--- test-java-lib/src/main/java/com/uber/lib/Varargs.java
+++ test-java-lib/src/main/java/com/uber/lib/Varargs.java
@@ -7,4 +7,6 @@ public class Varargs {
   public Varargs(@Nullable String... args) {}
 
   public static void typeUse(String @org.jspecify.annotations.Nullable ... args) {}
+
+  public static void typeUseNullableElementsJSpecify(@org.jspecify.annotations.Nullable String... args) {}
 }
